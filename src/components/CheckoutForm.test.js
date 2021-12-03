9777938c1bdb366c98c7887390df75efb0aa15d5
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-03 03:21:21 -0500

    completed testing for checkout form in checkoutform.test.js

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,42 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+  // Arrange
+  render (<CheckoutForm />);
 
-test("shows success message on submit with form details", () => {});
+});
+
+test("shows success message on submit with form details", async () => {
+  // Arrange
+  render (<CheckoutForm />);
+
+  // Act
+  const firstNameField = screen.getByLabelText(/first name:/i);
+  const lastNameField = screen.getByLabelText(/last name:/i);
+  const addressField = screen.getByLabelText(/address:/i);
+  const cityField = screen.getByLabelText(/city:/i);
+  const stateField = screen.getByLabelText(/state:/i);
+  const zipField = screen.getByLabelText(/zip:/i);
+
+  const checkoutButton = screen.getByRole("button");
+
+  userEvent.type(firstNameField, "Benjamin");
+  userEvent.type(lastNameField, "Craddock");
+  userEvent.type(addressField, "1 BloomTech Lane");
+  userEvent.type(cityField, "LambdaLand");
+  userEvent.type(stateField, "LambdaVerse");
+  userEvent.type(zipField, "77777");
+
+  userEvent.click(checkoutButton);
+
+  // Assert
+  const successMessage = await screen.findByText(/you have ordered some plants! woo-hoo!/i);
+  expect(successMessage).toBeInTheDocument();
+
+});
